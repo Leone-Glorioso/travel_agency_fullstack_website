666e880e41d344a9a31105ea9bdc7fd145925133
Konstantinos Leontiadis <123456kost@gmail.com>
2023-08-02 11:45:31 +0300

    Completed Repositories

---
+++ travel_website_2_backend/src/main/java/com/travel_website/travel_website_2_backend/Repository/LocationRepository.java
@@ -0,0 +1,19 @@
+package com.travel_website.travel_website_2_backend.Repository;
+
+import com.travel_website.travel_website_2_backend.Models.Location;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface LocationRepository extends JpaRepository<Location, Integer> {
+    boolean existsById(int id);
+
+    boolean existsByAddress(String address);
+
+    Optional<Location> findById(int id);
+
+    Optional<Location> findByAddress(String address);
+
+    List<Location> findByNeighbourhood(String neighbourhood);
+}
---
+++ travel_website_2_backend/src/main/java/com/travel_website/travel_website_2_backend/Repository/PhotosRepository.java
@@ -0,0 +1,12 @@
+package com.travel_website.travel_website_2_backend.Repository;
+
+import com.travel_website.travel_website_2_backend.Models.Photos;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PhotosRepository extends JpaRepository<Photos, Integer> {
+    Photos findById(int id);
+
+    Photos findByPath(String path);
+
+    boolean existsByPath(String path);
+}
