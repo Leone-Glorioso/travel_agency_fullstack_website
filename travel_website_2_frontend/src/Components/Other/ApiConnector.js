a545b3ca4e71cdb8adc1514c3be11353534dd5bf
Dimitris_an <101252889+sdi2100002@users.noreply.github.com>
2023-08-18 17:19:28 +0300

    ApiConnector done

--- travel_website_2_frontend/src/Components/Other/ApiConnector.js
+++ travel_website_2_frontend/src/Components/Other/ApiConnector.js
@@ -1,5 +1,37 @@
 import axios from 'axios'
 import {config} from "../../Constants";
+import {parseJwt} from "./Helpers";
+
+export const ApiConnector={
+    authenticate,
+    signUp,
+    numberOfUsers,
+    numberOfReservations,
+    getUsers,
+    deleteUser,
+    getReservations,
+    getReservationsByBookerAndRoom,
+    getReservation,
+    createReservation,
+    deleteReservation,
+    getMyReservations,
+    getReservationsOfClient,
+    getMyReservation,
+    getReservationOfClient,
+    getReservationsOfMyRoom,
+    getReservationsOfLandlordRoom,
+    getReservationOfMyRoom,
+    getReservationOfLandlordRoom,
+    getRooms,
+    createRoom,
+    deleteRoom,
+    getRoom,
+    getMyRooms,
+    getRoomsByLandlord,
+    getMyRoom,
+    getRoomByLandlord,
+    getUserMe
+}
 
 function authenticate(username,password){
     return instance.post('auth/authenticate',{username,password},{headers:{'Content-type':'application/json'}})
@@ -132,9 +164,19 @@ const instance = axios.create({
     baseURL: config.url.API_BASE_URL
 })
 
-
-
-
+instance.interceptors.request.use(function (config) {
+    // Terminates when the login ends
+    if (config.headers.Authorization) {
+        const token = config.headers.Authorization.split(' ')[1]
+        const data = parseJwt(token)
+        if (Date.now() > data.exp * 1000) {
+            window.location.href = "/login"
+        }
+    }
+    return config
+}, function (error) {
+    return Promise.reject(error)
+})
 
 
 function bearerAuth(user) {
