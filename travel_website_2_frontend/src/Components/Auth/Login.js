478301786b534d95e12662d8ed9379cf981c94aa
Dimitris_an <101252889+sdi2100002@users.noreply.github.com>
2023-08-22 18:58:57 +0300

    login Error fixed

--- travel_website_2_frontend/src/Components/Auth/Login.js
+++ travel_website_2_frontend/src/Components/Auth/Login.js
@@ -2,103 +2,205 @@ import React ,{useState} from 'react'
 import {useAuth} from "./contex";
 import {ApiConnector} from "../Other/ApiConnector";
 import {handleLogError, parseJwt} from "../Other/Helpers";
-//import * as PropTypes from "prop-types";
+import * as PropTypes from "prop-types";
 import { NavLink, Navigate } from 'react-router-dom'
 import { Button, Form, Grid, Segment, Message } from 'semantic-ui-react'
 
 import "./Login.css"
 
-/*function Grid(props) {
-    return null;
-}
+function Login() {
+    const Auth = useAuth();
+    const isLogged = Auth.userIsAuthenticated();
 
-Grid.propTypes = {
-    textAlign: PropTypes.string,
-    children: PropTypes.node
-};*/
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [isError, setIsError] = useState(false);
 
-function Login(){
-    const Auth=useAuth()
-    const isLogged=Auth.userIsAuthenticated()
-
-    const [username,setUsername]=useState('')
-    const [password,setPassword]=useState('')
-    const [isError,setIsError]=useState(false)
-
-    const handleInputChange= (e,{name,value}) => {
-        if (name==='username'){
-            setUsername(value)
-        }else if(name==='password'){
-            setPassword(value)
+    const handleInputChange = (e, { name, value }) => {
+        if (name === 'username') {
+            setUsername(value);
+        } else if (name === 'password') {
+            setPassword(value);
         }
-    }
+    };
 
-    const handleSubmit=async (e)=>{
-        e.preventDefault()
+    const handleSubmit = async (e) => {
+        e.preventDefault();
 
-        if(!(username && password)){
-            setIsError(true)
-            return
+        if (!(username && password)) {
+            setIsError(true);
+            return;
         }
 
-        try{
-            const response=await ApiConnector.authenticate(username,password)
-            const {accessToken} =response.data
-            const data=parseJwt(accessToken)
-            const authUser={data,accessToken}
+        try {
+            const response = await ApiConnector.authenticate(username, password);
+            const { accessToken, ...userData } = response.data; // Assuming user data is in response
 
-            Auth.userLogin(authUser)
+            Auth.userLogin(userData, accessToken);
 
-            setUsername('')
-            setPassword('')
-            setIsError(false)
-        } catch (error){
-            handleLogError(error)
-            setIsError(true)
+            setUsername('');
+            setPassword('');
+            setIsError(false);
+        } catch (error) {
+            handleLogError(error);
+            setIsError(true);
         }
-    }
+    };
 
-    if(isLogged){
-        return <Navigate to={'/'} />
+    if (isLogged) {
+        return <Navigate to={'/'} />;
     }
 
     return (
-        <Grid textAlign='center' className={'app'}>
+        <Grid textAlign="center" className={'app'}>
             <Grid.Column style={{ maxWidth: 450 }}>
-                <Form size='large' onSubmit={handleSubmit} className={'login-form'}>
+                <Form size="large" onSubmit={handleSubmit} className={'login-form'}>
                     <Segment>
                         <Form.Input
                             fluid
                             autoFocus
-                            name='username'
-                            icon='user'
-                            iconPosition='left'
-                            placeholder='Username'
+                            name="username"
+                            icon="user"
+                            iconPosition="left"
+                            placeholder="Username"
                             value={username}
                             onChange={handleInputChange}
                             className={'input-container'}
                         />
                         <Form.Input
                             fluid
-                            name='password'
-                            icon='lock'
-                            iconPosition='left'
-                            placeholder='Password'
-                            type='password'
+                            name="password"
+                            icon="lock"
+                            iconPosition="left"
+                            placeholder="Password"
+                            type="password"
                             value={password}
                             onChange={handleInputChange}
                             className={'input-container'}
                         />
-                        <Button color='violet' fluid size='large' className={'button-container'}>Login</Button>
+                        <Button color="violet" fluid size="large" className={'button-container'}>
+                            Login
+                        </Button>
                     </Segment>
                 </Form>
-                <Message>{`Don't have already an account? `}
-                    <NavLink to="/signup" color='violet' as={NavLink}>Sign Up</NavLink>
+                <Message>
+                    {`Don't have already an account? `}
+                    <NavLink to="/signup" color="violet" as={NavLink}>
+                        Sign Up
+                    </NavLink>
                 </Message>
                 {isError && <Message negative>The username or password provided are incorrect!</Message>}
             </Grid.Column>
         </Grid>
-    )
+    );
 }
 
-export default Login
+export default Login;
+
+
+
+
+// import React ,{useState} from 'react'
+// import {useAuth} from "./contex";
+// import {ApiConnector} from "../Other/ApiConnector";
+// import {handleLogError, parseJwt} from "../Other/Helpers";
+// //import * as PropTypes from "prop-types";
+// import { NavLink, Navigate } from 'react-router-dom'
+// import { Button, Form, Grid, Segment, Message } from 'semantic-ui-react'
+//
+// import "./Login.css"
+//
+// /*function Grid(props) {
+//     return null;
+// }
+//
+// Grid.propTypes = {
+//     textAlign: PropTypes.string,
+//     children: PropTypes.node
+// };*/
+//
+// function Login(){
+//     const Auth=useAuth()
+//     const isLogged=Auth.userIsAuthenticated()
+//
+//     const [username,setUsername]=useState('')
+//     const [password,setPassword]=useState('')
+//     const [isError,setIsError]=useState(false)
+//
+//     const handleInputChange= (e,{name,value}) => {
+//         if (name==='username'){
+//             setUsername(value)
+//         }else if(name==='password'){
+//             setPassword(value)
+//         }
+//     }
+//
+//     const handleSubmit=async (e)=>{
+//         e.preventDefault()
+//
+//         if(!(username && password)){
+//             setIsError(true)
+//             return
+//         }
+//
+//         try{
+//             const response=await ApiConnector.authenticate(username,password)
+//             const {accessToken} =response.data
+//             const data=parseJwt(accessToken)
+//             const authUser={data,accessToken}
+//
+//             Auth.userLogin(authUser)
+//
+//             setUsername('')
+//             setPassword('')
+//             setIsError(false)
+//         } catch (error){
+//             handleLogError(error)
+//             setIsError(true)
+//         }
+//     }
+//
+//     if(isLogged){
+//         return <Navigate to={'/'} />
+//     }
+//
+//     return (
+//         <Grid textAlign='center' className={'app'}>
+//             <Grid.Column style={{ maxWidth: 450 }}>
+//                 <Form size='large' onSubmit={handleSubmit} className={'login-form'}>
+//                     <Segment>
+//                         <Form.Input
+//                             fluid
+//                             autoFocus
+//                             name='username'
+//                             icon='user'
+//                             iconPosition='left'
+//                             placeholder='Username'
+//                             value={username}
+//                             onChange={handleInputChange}
+//                             className={'input-container'}
+//                         />
+//                         <Form.Input
+//                             fluid
+//                             name='password'
+//                             icon='lock'
+//                             iconPosition='left'
+//                             placeholder='Password'
+//                             type='password'
+//                             value={password}
+//                             onChange={handleInputChange}
+//                             className={'input-container'}
+//                         />
+//                         <Button color='violet' fluid size='large' className={'button-container'}>Login</Button>
+//                     </Segment>
+//                 </Form>
+//                 <Message>{`Don't have already an account? `}
+//                     <NavLink to="/signup" color='violet' as={NavLink}>Sign Up</NavLink>
+//                 </Message>
+//                 {isError && <Message negative>The username or password provided are incorrect!</Message>}
+//             </Grid.Column>
+//         </Grid>
+//     )
+// }
+//
+// export default Login
--- travel_website_2_frontend/src/Components/Auth/contex.js
+++ travel_website_2_frontend/src/Components/Auth/contex.js
@@ -1,67 +1,131 @@
-import {createContext, useContext, useEffect, useState} from "react";
 
 
-const AuthContext=createContext()
+import { createContext, useContext, useEffect, useState } from "react";
 
-function AuthProvider({children}){
 
-    const [user,setUser]=useState(null)
+const AuthContext = createContext();
 
-    useEffect(()=>{
-        const storedUser=JSON.parse(localStorage.getItem('user'))
-        setUser(storedUser)
-    }, [])
+function AuthProvider({ children }) {
+    const [user, setUser] = useState(null);
 
-    const getUser = () => {
-        return JSON.parse(localStorage.getItem('user'))
-    }
+    useEffect(() => {
+        const storedUser = JSON.parse(localStorage.getItem("user"));
+        if (storedUser) {
+            // Check token expiry and update user state accordingly
+            if (Date.now() < storedUser.exp * 1000) {
+                setUser(storedUser);
+            } else {
+                userLogout();
+            }
+        }
+    }, []);
 
     const userIsAuthenticated = () => {
-        let storedUser=localStorage.getItem('user')
-        console.log(user)
-        if (!storedUser){
-            console.log('cy@')
-            return false
-        }
-        storedUser=JSON.parse(storedUser)
+        return user !== null;
+    };
 
-        if (Date.now() > storedUser.data.exp * 1000){
-            userLogout()
-            return false
-        }
-        return true
+    function GetUserName(){
+        const Auth=useAuth()
+        return Auth.user? Auth.user.username: null;
     }
 
-    const userLogin=user => {
-        localStorage.setItem('user',JSON.stringify(user))
-        setUser(user)
-    }
+    const userLogin = (user, accessToken) => {
+        localStorage.setItem("user", JSON.stringify({ user, accessToken }));
+        setUser({ user, accessToken });
+    };
 
-    const userLogout=() => {
-        localStorage.removeItem('user')
-        setUser(null)
-    }
+
+
+    const userLogout = () => {
+        // Clear the user data from local storage
+        localStorage.removeItem("user");
+        setUser(null);
+    };
 
     const contextValue = {
         user,
-        getUser,
         userIsAuthenticated,
         userLogin,
         userLogout,
-    }
+    };
 
     return (
         <AuthContext.Provider value={contextValue}>
             {children}
         </AuthContext.Provider>
-    )
-
+    );
 }
 
-export default AuthContext
+export default AuthContext;
 
-export function useAuth(){
-    return useContext(AuthContext)
+export function useAuth() {
+    return useContext(AuthContext);
 }
 
-export {AuthProvider}
+export { AuthProvider };
+
+
+// const AuthContext=createContext()
+//
+// function AuthProvider({children}){
+//
+//     const [user,setUser]=useState(null)
+//
+//     useEffect(()=>{
+//         const storedUser=JSON.parse(localStorage.getItem('user'))
+//         setUser(storedUser)
+//     }, [])
+//
+//     const getUser = () => {
+//         return JSON.parse(localStorage.getItem('user'))
+//     }
+//
+//     const userIsAuthenticated = () => {
+//         let storedUser=localStorage.getItem('user')
+//         console.log(user)
+//         if (!storedUser){
+//             console.log('cy@')
+//             return false
+//         }
+//         storedUser=JSON.parse(storedUser)
+//
+//         if (Date.now() > storedUser.data.exp * 1000){
+//             userLogout()
+//             return false
+//         }
+//         return true
+//     }
+//
+//     const userLogin=user => {
+//         localStorage.setItem('user',JSON.stringify(user))
+//         setUser(user)
+//     }
+//
+//     const userLogout=() => {
+//         localStorage.removeItem('user')
+//         setUser(null)
+//     }
+//
+//     const contextValue = {
+//         user,
+//         getUser,
+//         userIsAuthenticated,
+//         userLogin,
+//         userLogout,
+//     }
+//
+//     return (
+//         <AuthContext.Provider value={contextValue}>
+//             {children}
+//         </AuthContext.Provider>
+//     )
+//
+// }
+//
+// export default AuthContext
+//
+// export function useAuth(){
+//     return useContext(AuthContext)
+// }
+//
+// export {AuthProvider}
