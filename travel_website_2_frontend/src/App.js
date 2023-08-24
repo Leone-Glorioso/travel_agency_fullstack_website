3e04122c0df5413c4b4154a4bcd71e822423f5ae
Dimitris_an <101252889+sdi2100002@users.noreply.github.com>
2023-08-24 19:46:23 +0300

    some styling

--- travel_website_2_frontend/src/App.js
+++ travel_website_2_frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route, Navigate, Link} from 'react-router-dom'
 import './App.css';
 import {AuthProvider} from "./Components/Auth/contex";
 import Home from "./Components/Home/Home";
@@ -10,14 +10,35 @@ function App() {
   return (
       <AuthProvider>
         <Router>
+            <div className={"GreekTours"}>
+                <header>
+                    <h1>
+                        Welcome to our Website
+                    </h1>
+                </header>
+                <nav>
+                    <ul>
+                        <li>  <Link to={"/signup"}>Sign Up</Link></li>
+                        <li> <Link to={"/login"}> Login </Link></li>
+                    </ul>
+                </nav>
+                
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/login' element={<Login />} />
+                    <Route path='/signup' element={<SingUp />} />
+                    <Route path='/profile' element={<Profile />} />
+                    <Route path="*" element={<Navigate to="/" />} />
+                </Routes>
+            </div>
           {/*<Navbar />*/}
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/signup' element={<SingUp />} />
-            <Route path='/profile' element={<Profile />} />
-            <Route path="*" element={<Navigate to="/" />} />
-          </Routes>
+          {/*<Routes>*/}
+          {/*  <Route path='/' element={<Home />} />*/}
+          {/*  <Route path='/login' element={<Login />} />*/}
+          {/*  <Route path='/signup' element={<SingUp />} />*/}
+          {/*  <Route path='/profile' element={<Profile />} />*/}
+          {/*  <Route path="*" element={<Navigate to="/" />} />*/}
+          {/*</Routes>*/}
         </Router>
       </AuthProvider>
   );
