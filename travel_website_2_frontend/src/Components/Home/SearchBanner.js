4d1fb3725ef65c5e68e995a8fdc643690bcd2f00
Dimitris_an <101252889+sdi2100002@users.noreply.github.com>
2023-08-26 20:40:49 +0300

    Banner Created with a photo for background

---
+++ travel_website_2_frontend/src/Components/Home/SearchBanner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SearchBanner.css'
+
+function SearchBanner () {
+    return (
+        <div className={'SearchBanner'}>
+            <h1>Banner</h1>
+        </div>
+    )
+}
+
+export default SearchBanner;
