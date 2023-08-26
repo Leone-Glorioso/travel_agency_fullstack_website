cc7593176e2bfce1dcaacf43f908b06612ee594f
Dimitris_an <101252889+sdi2100002@users.noreply.github.com>
2023-08-26 21:02:25 +0300

    Search Button Started

--- travel_website_2_frontend/src/Components/Home/SearchBanner.js
+++ travel_website_2_frontend/src/Components/Home/SearchBanner.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import './SearchBanner.css'
+import { IconButton } from '@mui/material';
 
 function SearchBanner () {
     return (
         <div className={'SearchBanner'}>
             <h1>Banner</h1>
+            <div className={'ButtonSearchNearBy'}>
+                <IconButton variant={'outlined'}>
+                    Explore Nearby
+                </IconButton>
+            </div>
         </div>
     )
 }
