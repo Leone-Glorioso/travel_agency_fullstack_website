89cb5c30ac9ad1a4dcb916b7e16510b7f33d25bd
Dimitris_an <101252889+sdi2100002@users.noreply.github.com>
2023-08-28 19:31:18 +0300

    Search date started

--- travel_website_2_frontend/src/Components/Home/SearchBanner.js
+++ travel_website_2_frontend/src/Components/Home/SearchBanner.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './SearchBanner.css'
 import { IconButton } from '@mui/material';
 
 function SearchBanner () {
+    const [showSearch,setShowSearch]=useState(false);
     return (
         <div className={'SearchBanner'}>
-            <h1>Banner</h1>
-            <div className={'ButtonSearchNearBy'}>
+            <div className={'Search'}>
+                {showSearch && <h1>SHOW DATE</h1>}
+                <IconButton onClick={()=>setShowSearch(!showSearch)} className={'SearchButton'} variant={'outlined'}>
+                    Search Dates
+                </IconButton>
+            </div>
+            <div className={'SearchBannerInfo'}>
+                <h1>Live your Myth in our modern homes</h1>
+                <h2>Plan your vacations worldwide</h2>
+            </div>
                 <IconButton variant={'outlined'}>
                     Explore Nearby
                 </IconButton>
-            </div>
         </div>
     )
 }
