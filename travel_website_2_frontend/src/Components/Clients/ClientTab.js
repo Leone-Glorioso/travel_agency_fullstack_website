2ee17a526617100d0cfdc2dbbea3463db8d04d3a
Konstantinos Leontiadis <123456kost@gmail.com>
2023-08-31 12:21:53 +0300

    added tabs (not personalized to site)

---
+++ travel_website_2_frontend/src/Components/Clients/ClientTab.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Tab } from 'semantic-ui-react'
+import MyReservationsTableClient from "./MyReservationsTable";
+
+function ClientTab(props) {
+    const { handleInputChange } = props
+    const { isReservationsLoading, reservations, reservationIdSearch, handleDeleteReservation, handleSearchReservation } = props
+
+    const panes = [
+        {
+            menuItem: { key: 'reservations', icon: 'bookmark', content: 'Reservations' },
+            render: () => (
+                <Tab.Pane loading={isReservationsLoading}>
+                    <MyReservationsTableClient
+                        reservations={reservations}
+                        reservationIdSearch={reservationIdSearch}
+                        handleDeleteReservation={handleDeleteReservation}
+                        handleInputChange={handleInputChange}
+                        handleSearchReservation={handleSearchReservation}
+                    />
+                </Tab.Pane>
+            )
+        }
+    ]
+
+    return (
+        <Tab menu={{ attached: 'top' }} panes={panes} />
+    )
+}
+
+export default ClientTab
---
+++ travel_website_2_frontend/src/Components/LandlordClients/LandlordClientTab.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import { Tab } from 'semantic-ui-react'
+import ReservationsTableFromLandlordClient from "./MyReservationsTable";
+import ReservationsTableToLandlordClient from "./ReservationsOfMyRoomsTable";
+import RoomsTableLandlordClient from "./RoomsTable";
+
+function LandlordTab(props) {
+    const { handleInputChange } = props
+    const { isRoomsLoading, rooms, roomIdSearch, handleDeleteRoom, handleSearchRoom } = props
+    const { isReservationsLoading_to, reservations_to, reservationIdSearch_to, handleDeleteReservation_to, handleSearchReservation_to } = props
+    const { isReservationsLoading_from, reservations_from, reservationIdSearch_from, handleDeleteReservation_from, handleSearchReservation_from } = props
+
+    const panes = [
+        {
+            menuItem: { key: 'rooms', icon: 'bed', content: 'Rooms' },
+            render: () => (
+                <Tab.Pane loading={isRoomsLoading}>
+                    <RoomsTableLandlordClient
+                        rooms={rooms}
+                        roomIdSearch={roomIdSearch}
+                        handleDeleteRoom={handleDeleteRoom}
+                        handleInputChange={handleInputChange}
+                        handleSearchRoom={handleSearchRoom}
+                    />
+                </Tab.Pane>
+            )
+        },
+        {
+            menuItem: { key: 'reservations', icon: 'bookmark', content: 'Reservations' },
+            render: () => (
+                <Tab.Pane loading={isReservationsLoading_to}>
+                    <ReservationsTableToLandlordClient
+                        reservations={reservations_to}
+                        reservationIdSearch={reservationIdSearch_to}
+                        handleDeleteReservation={handleDeleteReservation_to}
+                        handleInputChange={handleInputChange}
+                        handleSearchReservation={handleSearchReservation_to}
+                    />
+                </Tab.Pane>
+            )
+        },
+        {
+            menuItem: { key: 'reservations', icon: 'bookmark', content: 'Reservations' },
+            render: () => (
+                <Tab.Pane loading={isReservationsLoading_from}>
+                    <ReservationsTableToLandlordClient
+                        reservations={reservations_from}
+                        reservationIdSearch={reservationIdSearch_from}
+                        handleDeleteReservation={handleDeleteReservation_from}
+                        handleInputChange={handleInputChange_from}
+                        handleSearchReservation={handleSearchReservation_from}
+                    />
+                </Tab.Pane>
+            )
+        }
+    ]
+
+    return (
+        <Tab menu={{ attached: 'top' }} panes={panes} />
+    )
+}
+
+export default LandlordTab
