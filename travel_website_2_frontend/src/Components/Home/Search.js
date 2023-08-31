c74732d55b6f517fb8ea8739961ead6ca173cf98
Dimitris_an <101252889+sdi2100002@users.noreply.github.com>
2023-08-31 12:41:13 +0300

    Search #3

--- travel_website_2_frontend/src/Components/Home/Search.js
+++ travel_website_2_frontend/src/Components/Home/Search.js
@@ -10,10 +10,35 @@ import {ApiConnector} from "../Other/ApiConnector";
 
 
 function Search(){
+
     const [startDate,setStartDate]=useState(new Date())
     const [endDate,setEndDate]=useState(new Date())
     const [numOfGuests,setNumOfGuests]=useState(2)
-    const [latitude,setLatitude]=useState(-30.0)
+    const [latitude, setLatitude] = useState(-50.0);
+    const [longitude, setLongitude] = useState(-50.0);
+    const [range, setRange] = useState(10);
+    const [typeOfRoom, setTypeOfRoom] = useState("private_room");
+    const [startNumOfBeds, setStartNumOfBeds] = useState(1);
+    const [endNumOfBeds, setEndNumOfBeds] = useState(10);
+    const [startNumOfBaths, setStartNumOfBaths] = useState(1);
+    const [endNumOfBaths, setEndNumOfBaths] = useState(10);
+    const [startNumOfBedrooms, setStartNumOfBedrooms] = useState(1);
+    const [endNumOfBedrooms, setEndNumOfBedrooms] = useState(10);
+    const [livingRoom, setLivingRoom] = useState(true);
+    const [startArea, setStartArea] = useState(100);
+    const [endArea, setEndArea] = useState(1000);
+    const [smoking, setSmoking] = useState(true);
+    const [pets, setPets] = useState(true);
+    const [events, setEvents] = useState(true);
+    const [internet, setInternet] = useState(true);
+    const [cooling, setCooling] = useState(true);
+    const [heating, setHeating] = useState(true);
+    const [kitchen, setKitchen] = useState(true);
+    const [tv, setTV] = useState(true);
+    const [parking, setParking] = useState(true);
+    const [elevator, setElevator] = useState(true);
+
+
 
     const selectionRange={
         startDate:startDate,
@@ -26,33 +51,128 @@ function Search(){
         setEndDate(ranges.selection.endDate)
     }
 
+    const handleSetLatitude=(value) => {
+        console.log(value)
+        setLatitude(value)
+    }
+
+    const handleSetLongtitude=(value) =>{
+        console.log(value)
+        setLongitude(value)
+    }
+
+    const handleSetRange = (value) => {
+        setRange(value);
+    };
+
+    const handleSetTypeOfRoom = (value) => {
+        setTypeOfRoom(value);
+    };
+
+    const handleSetStartNumOfBeds = (value) => {
+        setStartNumOfBeds(value);
+    };
+
+    const handleSetEndNumOfBeds = (value) => {
+        setEndNumOfBeds(value);
+    };
+
+    const handleSetStartNumOfBaths = (value) => {
+        setStartNumOfBaths(value);
+    };
+
+    const handleSetEndNumOfBaths = (value) => {
+        setEndNumOfBaths(value);
+    };
+
+    const handleSetStartNumOfBedrooms = (value) => {
+        setStartNumOfBedrooms(value);
+    };
+
+    const handleSetEndNumOfBedrooms = (value) => {
+        setEndNumOfBedrooms(value);
+    };
+
+    const handleSetLivingRoom = (value) => {
+        setLivingRoom(value);
+    };
+
+    const handleSetStartArea = (value) => {
+        setStartArea(value);
+    };
+
+    const handleSetEndArea = (value) => {
+        setEndArea(value);
+    };
+
+    const handleSetSmoking = (value) => {
+        setSmoking(value);
+    };
+
+    const handleSetPets = (value) => {
+        setPets(value);
+    };
+
+    const handleSetEvents = (value) => {
+        setEvents(value);
+    };
+
+    const handleSetInternet = (value) => {
+        setInternet(value);
+    };
+
+    const handleSetCooling = (value) => {
+        setCooling(value);
+    };
+
+    const handleSetHeating = (value) => {
+        setHeating(value);
+    };
+
+    const handleSetKitchen = (value) => {
+        setKitchen(value);
+    };
+
+    const handleSetTV = (value) => {
+        setTV(value);
+    };
+
+    const handleSetParking = (value) => {
+        setParking(value);
+    };
+
+    const handleSetElevator = (value) => {
+        setElevator(value);
+    };
+
+
     const handleSearch = async () => {
         const searchRequest = {
-            latitude: -50.0,
-            longitude: -50.0,
-            range: 10,
+            latitude: latitude,
+            longitude: longitude,
+            range:range,
             start: startDate.toISOString(),
             end: endDate.toISOString(),
-            typeofroom: "private_room",
-            start_numOfBeds: 1,
-            end_numOfBeds: 10,
-            start_numOfBaths: 1,
-            end_numOfBaths: 10,
-            start_numOfBedrooms: 1,
-            end_numOfBedrooms: 10,
-            livingRoom: true,
-            start_area: 100,
-            end_area: 1000,
-            smoking: true,
-            pets: true,
-            events: true,
-            internet: true,
-            cooling: true,
-            heating: true,
-            kitchen: true,
-            tv: true,
-            parking: true,
-            elevator: true,
+            typeofroom: typeOfRoom,
+            start_numOfBeds: startNumOfBeds,
+            end_numOfBeds:endNumOfBeds,
+            start_numOfBaths: startNumOfBaths,
+            end_numOfBaths: endNumOfBaths,
+            start_numOfBedrooms: startNumOfBedrooms,
+            end_numOfBedrooms:endNumOfBedrooms,
+            livingRoom:livingRoom,
+            start_area: startArea,
+            end_area: endArea,
+            smoking: smoking,
+            pets: pets,
+            events: events,
+            internet: internet,
+            cooling: cooling,
+            heating: heating,
+            kitchen: kitchen,
+            tv: tv,
+            parking: parking,
+            elevator: elevator,
             first_element: 1,
             last_element: 1,
             flags: "beds,bedrooms,baths,dates,location,area,livingRoom,smoking,pets,events,internet,cooling,heating,kitchen,tv,parking,elevator"
@@ -87,14 +207,26 @@ function Search(){
             </h1>
             <input
                 min={0}
+                value={numOfGuests}
                 defaultValue={2}
                 type={"number"}
                 onChange={(e)=>setNumOfGuests(e.target.value)}
             />
             <div className={'SearchButtons'}>
-                <button onClick={() => console.log("Latitude:", -50.0)}>Latitude</button>
-                <button onClick={() => console.log("Latitude:", -50.0)}>Latitude</button>
-                <button onClick={() => console.log("Longitude:", -50.0)}>Longitude</button>
+                <div className={'SearchInputs'}>
+                    <label>Latitude:</label>
+                    <input
+                        type="number"
+                        value={latitude}
+                        onChange={(e) => handleSetLatitude(e.target.value)}
+                    />
+                    <input
+                        type="number"
+                        value={longitude}
+                        onChange={(e) => handleSetLongtitude(e.target.value)}
+                    />
+                </div>
+
                 <button onClick={() => console.log("Range:", 10)}>Range</button>
                 <button onClick={() => console.log("Start Date:", startDate.toISOString())}>Start Date</button>
                 <button onClick={() => console.log("End Date:", endDate.toISOString())}>End Date</button>
@@ -118,7 +250,6 @@ function Search(){
                 <button onClick={() => console.log("TV:", true)}>TV</button>
                 <button onClick={() => console.log("Parking:", true)}>Parking</button>
                 <button onClick={() => console.log("Elevator:", true)}>Elevator</button>
-
             </div>
             <IconButton onClick={handleSearch}>Search Room</IconButton>
 
