1525780262c0c2b9a312a42491264f72cb3bdf90
Konstantinos Leontiadis <123456kost@gmail.com>
2023-09-04 19:14:11 +0300

    created some new components don't think they work

---
+++ travel_website_2_frontend/src/Components/UploadImage/uploadActions.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+export const uploadImage = (imageData, type) => async dispatch => {
+    if (imageData.entries().next().value[1] !== null) {
+        const response = await axios.post(axios.defaults.baseURL + `/api/upload/image`, imageData, {
+            onUploadProgress:progressEvent => {
+                console.log("Uploading : " + ((progressEvent.loaded / progressEvent.total) * 100).toString() + "%")
+            }
+        });
+        dispatch({
+            type: type,
+            payload: response.data
+        });
+    }
+};
